312a0d21f4f5c3b9fdbeff75170e0a4b8c04fae3
Edgar Fernandez <edgar1626fernandez@gmail.com>
2024-02-23 00:59:52 -0500

    ssh change0 commit

---
+++ MATLABsend.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+#SBATCH --gpus-per-node=1
+#SBATCh --nodes=1
+#SBATCH --partition=gamerpcs
+#SBATCH --nodelist=worker1
+#SBATCH --output="log.out"
+
+srun matlab -nosplash -nodesktop -nodisplay -r "gendata; exit"
+
