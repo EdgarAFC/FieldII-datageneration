3bba19846cad13c935207036c60cbd5f16ccb825
Edgar Fernandez <edgar1626fernandez@gmail.com>
2024-03-07 19:57:20 -0500

    W1: parfor2 (185-200) commit

--- gendata2.m
+++ gendata2.m
@@ -35,7 +35,7 @@ randpos_axpos = h5read(file_path, '/axpos');
 M = 2;
 elapsed = 0;
 
-parfor (idx_simu = 167:184,M)
+parfor (idx_simu = 185:200,M)
     % Get phantom and cyst parameters
     r = possible_r(randpos_r(idx_simu));
     c = possible_c(randpos_c(idx_simu));
