93067bd4f987b1e47d4ca8d1f4cee2d760e192e5
Edgar Fernandez <edgar1626fernandez@gmail.com>
2024-03-17 00:40:30 -0500

    changed worker commit

--- MATLABW1-2send.sh
+++ MATLABW1-2send.sh
@@ -2,7 +2,7 @@
 #SBATCH --gpus-per-node=1
 #SBATCh --nodes=1
 #SBATCH --partition=gamerpcs
-#SBATCH --nodelist=worker1
+#SBATCH --nodelist=worker2
 #SBATCH --output="log2.out"
 
 srun matlab -nosplash -nodesktop -nodisplay -r "gendata2; exit"
