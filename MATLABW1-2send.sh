d8e681e01f2904bfa940ddb5b2c2127061134f11
Edgar Fernandez <edgar1626fernandez@gmail.com>
2024-03-20 16:09:22 -0500

    W1: parfor2 (501-502) and changing paths commit

--- MATLABW1-2send.sh
+++ MATLABW1-2send.sh
@@ -2,7 +2,7 @@
 #SBATCH --gpus-per-node=1
 #SBATCh --nodes=1
 #SBATCH --partition=gamerpcs
-#SBATCH --nodelist=worker2
+#SBATCH --nodelist=worker1
 #SBATCH --output="log2.out"
 
 srun matlab -nosplash -nodesktop -nodisplay -r "gendata2; exit"
